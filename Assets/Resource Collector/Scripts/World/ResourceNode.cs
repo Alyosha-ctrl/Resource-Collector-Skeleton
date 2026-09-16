ff78504eccd4dea6c0e33b14c7af79fd291bb7de
Alyosha-ctrl <alexeibere@gmail.com>
2026-09-16 14:21:26 -0700

    resource node bug fixes

--- Assets/Resource Collector/Scripts/World/ResourceNode.cs
+++ Assets/Resource Collector/Scripts/World/ResourceNode.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Unity.Netcode;
+using UnityEditor;
+using UnityEditor.UI;
 using UnityEngine;
 
 /*
@@ -22,43 +24,86 @@ public class ResourceNode : Interactable
     {
         base.OnNetworkSpawn();
 
-        // TODO Slice 8.1: on the server, set health to _startingHealth. Then
-        // subscribe to health changes and apply the current health.
+        
+
+        if(IsServer)
+        {
+            // TODO Slice 8.1: on the server, set health to _startingHealth. 
+            
+            _health.Value = _startingHealth;
+        }
+        // Then subscribe to health changes and apply the current health.
+        _health.OnValueChanged += HandleHealthChanged;
+        HandleHealthChanged(0, _health.Value);
+        ApplyHealth();
     }
 
     public override void OnNetworkDespawn()
     {
         // TODO Slice 8.4: unsubscribe from replicated health changes.
+        _health.OnValueChanged -= HandleHealthChanged;
         base.OnNetworkDespawn();
     }
 
     public override bool CanInteract(ObjectType heldType)
     {
         // TODO Slice 8.5: require a living node and an accepted tool.
-        return true;
+        if(_toolTypeRequired[0] == heldType && _health.Value > 0) return true;
+
+        return false;
     }
 
     protected override void Interact(PlayerHeldItem heldItem)
     {
         // TODO Slice 8.7: reduce health and play feedback. At zero, spawn
-        // _amountToSpawn copies of _producedPrefab with InstantiateAndSpawn.
-        // Place each on the ground with a small random XZ offset and random yaw.
         // </> end of Slice 8
+        HitFeedbackRpc();
+        HandleHealthChanged(_health.Value, _health.Value - 1);
+        if(_health.Value <= 0) SpawnResources();
+        ApplyHealth();
     }
 
     [Rpc(SendTo.ClientsAndHost)]
     void HitFeedbackRpc()
     {
         // TODO Slice 8.6: play the authored hit sound on each observer.
+        Debug.Log("Played Hit Sound");
+        AudioSource.PlayClipAtPoint(_audioClip, transform.position);
     }
 
     void HandleHealthChanged(int previousValue, int newValue)
     {
         // TODO Slice 8.3: apply replicated health locally.
+        _health.Value = newValue;
+        ApplyHealth();
     }
 
     void ApplyHealth()
     {
         // TODO Slice 8.2: hide depleted nodes and disable their collider.
+        if(_health.Value <= 0)
+        {
+            //Kill the node
+            gameObject.SetActive(false);
+            // NetworkObject.Despawn(true);
+        }
+    }
+
+    void SpawnResources()
+    {
+        // _amountToSpawn copies of _producedPrefab with InstantiateAndSpawn.
+        // Place each on the ground with a small random XZ offset and random yaw.
+        for (int i = 0; i < _amountToSpawn; i++)
+        {
+            //Randomize the position
+            Vector3 newPosition = transform.position;
+            newPosition.x += Random.Range(-1,1);
+            newPosition.z += Random.Range(-1,1);
+            Quaternion newRotation = transform.rotation;
+            newRotation.x += Random.Range(-30, 30);
+            newRotation.y += Random.Range(-30, 30);
+            newRotation.z += Random.Range(-30, 30);
+            NetworkObject.InstantiateAndSpawn(_producedPrefab.gameObject, NetworkManager, position: newPosition, rotation: newRotation);
+        }
     }
 }
